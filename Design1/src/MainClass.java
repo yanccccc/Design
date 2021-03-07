a0b71c6cfdc608e2c6cba2568bbb3edf55bb7b25
Yan <1016189150@qq.com>
2021-03-07 21:20:05 +0800

    设计模式

---
+++ Design1/src/MainClass.java
@@ -0,0 +1,8 @@
+public class MainClass {
+    public static void main(String[] args) {
+        DBUtil dbUtil = new DBUtil();
+        UserDAO userDAO = new UserDAO(dbUtil);
+        LoginForm loginForm = new LoginForm(userDAO,"userName","userPassword");
+        loginForm.validate();
+    }
+}
---
+++ Design1/src/UserDAO.java
@@ -0,0 +1,18 @@
+public class UserDAO {
+    private DBUtil db;
+    public UserDAO(DBUtil db){
+        this.db = db;
+    }
+
+    public boolean findUser(String userName,String userPassword){
+        db.geTConnection();
+        if((userName == "userName")&&(userPassword == "userPassword")){
+            System.out.println("登陆成功");
+            return true;
+        }else{
+            System.out.println("用户名或密码错误");
+            return false;
+        }
+
+    }
+}
