c6a856a9463812efd9d0314dd35a22a4b171c959
Yan <1016189150@qq.com>
2021-03-24 22:40:19 +0800

    第三次设计模式作业

---
+++ Design3/src/Client.java
@@ -0,0 +1,10 @@
+public class Client {
+    public static void main(String args[]) {
+        Image image;
+        ImageImp imp;
+        image = (Image)XMLUtil.getBean("image");
+        imp = (ImageImp)XMLUtil.getBean("os");
+        image.setImageImp(imp);
+        image.parseFile("长江大学");
+    }
+} 
