0338138d48b0b98bd91ac82703240f486cb2d32c
Yan <1016189150@qq.com>
2021-03-24 21:56:55 +0800

    第三次设计模式作业

---
+++ Design3/src/GIFImage.java
@@ -0,0 +1,9 @@
+//GIF格式图像：扩充抽象类
+public class GIFImage extends Image {
+    public void parseFile(String fileName) {
+        //模拟解析GIF文件并获得一个像素矩阵对象m;
+        Matrix m = new Matrix();
+        imp.doPaint(m);
+        System.out.println(fileName + "，格式为GIF。");
+    }
+}
---
+++ Design3/src/ImageImp.java
@@ -0,0 +1,4 @@
+//抽象操作系统实现类：实现类接口
+public interface ImageImp {
+    public void doPaint(Matrix m);  //显示像素矩阵m
+}
